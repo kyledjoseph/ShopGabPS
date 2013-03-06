08a465edf98a24e6df21ea1706024218ffa52b81
Tyler Matthews <tmatthews@fullsail.com>
2013-03-06 08:39:48 -0500

    configured production db credentials

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
+			'dsn'        => 'mysql:host=localhost;dbname=itemnation',
+			'username'   => 'root',
+			'password'   => 'RLJGwdbsEQ6pbJ6',
 		),
 	),
 );
