954ba1d8e3a855d574b92b23d3864bac09f13d2d
Tyler Matthews <tmatthews@fullsail.com>
2013-03-06 09:12:32 -0500

    additional info on admin product index

--- fuel/app/views/admin/products/index.php
+++ fuel/app/views/admin/products/index.php
@@ -1,6 +1,6 @@
-	
-	<h2>product index</h2>
-	<?= Html::anchor('admin/products/add', 'Add Product') ?>
+
+	<h2>products &gt; index</h2>
+	<?= Html::anchor('admin/products/add', 'add product') ?>
 
 	<hr>
 
@@ -8,9 +8,26 @@
 	<p>no products to display</p>
 	<?php else: ?>
 
+
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<th>name</th>
+				<th>brand</th>
+			</tr>
+		</thead>
+
 	<?php foreach ($products as $product): ?>
 
-	<?= Html::anchor("admin/products/view/{$product->id}", $product->name) ?>
+		<tbody>
+			<tr>
+				<td><?= Html::anchor("admin/products/view/{$product->id}", $product->name) ?></td>
+				<td><?= $product->brand ?></td>
+			</tr>
+		</tbody>
 
 	<?php endforeach; ?>
+
+	</table>
+
 	<?php endif; ?>
