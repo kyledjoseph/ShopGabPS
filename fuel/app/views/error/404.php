9d0fe5cfbb82001ec1325529d1016c2fee8a1bc6
Tyler Matthews <tmatthews@fullsail.com>
2013-03-12 12:20:04 -0400

    added default 404 title

--- fuel/app/views/error/404.php
+++ fuel/app/views/error/404.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title><?php echo $title; ?></title>
+	<title><?= isset($title) ? $title : '404: page not found' ?></title>
 	<?php echo Asset::css('bootstrap.css'); ?>
 	<style>
 		#logo{
@@ -42,7 +42,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="span16">
-				<h1><?php echo $title; ?> <small>We can't find that!</small></h1>
+				<h1><?= isset($title) ? $title : '404: page not found' ?></h1>
 				<hr>
 				<p>The controller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
 				<p>This view is located at <code>APPPATH/views/welcome/404.php</code>.</p>
