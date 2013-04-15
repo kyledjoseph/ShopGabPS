c7989c14498aed48c34465dd50602a8d35036841
tmatthewsdev <tmatthewsdev@gmail.com>
2013-04-15 15:47:46 -0400

    removed category view code

--- fuel/app/views/home/index.php
+++ fuel/app/views/home/index.php
@@ -1,13 +1,3 @@
 				<section id="products">
-					<ul>
-
-						<?php foreach ($categories as $category): ?>
-						
-						<li>
-							<?= Html::anchor($category->public_url(), "<figure></figure><span>{$category->name}</span>") ?>
-						</li>
-
-						<?php endforeach; ?>
-
-					</ul>
+					
 				</section>
