5e2982526fb51d829a63f08c379dcb7fb285a109
tmatthewsdev <tmatthewsdev@gmail.com>
2013-05-06 01:25:24 -0400

    basic admin authentication

--- fuel/app/classes/controller/admin.php
+++ fuel/app/classes/controller/admin.php
@@ -8,6 +8,7 @@ class Controller_Admin extends Controller_App
 	{
 		parent::before();
 
+		$this->admin_auth();
 		$this->template->active_nav = 'dashboard';
 	}
 
@@ -15,4 +16,25 @@ class Controller_Admin extends Controller_App
 	{
 		$this->template->body = View::forge('admin/dashboard/index');
 	}
+
+
+	/**
+	 * Require admin privileges to access 
+	 */
+	protected function admin_auth($auth_users = array())
+	{
+		$this->require_auth();
+		
+		$super_admins = array('4');
+		if (in_array($this->user->id, $super_admins))
+		{
+			return true;
+		}
+
+		if (empty($auth_users) or ! in_array($this->user->id, $auth_users))
+		{
+			$this->redirect('user/login', 'info', 'You must be logged in to do that.');
+		}
+	}
+
 }
