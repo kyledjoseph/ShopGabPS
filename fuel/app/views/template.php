9d6cb3f243330e50a1059f0840f35bc0b9ca3e2c
Dinko <dinkoslk55@yahoo.com>
2013-05-05 20:51:43 -0500

    fix footer input for email

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -176,7 +176,7 @@
 					<h3>Invite your friends</h3>
 
 					<?= Form::open(array('action' => 'user/invite', 'method' => 'POST')) ?>
-						<input name="emails" type="text" placeholder="yourfriend@email.com">
+						<input name="emails" type="email" placeholder="yourfriend@email.com">
 						<sub>* Add additional emails by a comma.</sub>
 						<button type="submit">Invite Now</button>
 					</form>
