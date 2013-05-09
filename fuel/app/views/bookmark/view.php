ffe5b53f52aa81140cc810c2ee02a9f40db1d779
Dinko <dinkoslk55@yahoo.com>
2013-05-08 21:54:34 -0500

    .

--- fuel/app/views/bookmark/view.php
+++ fuel/app/views/bookmark/view.php
@@ -4,8 +4,9 @@
 		<img class="product-image" id="1" class="image" src="http://placehold.it/200x200">
 		<div class="gallery">
 		</div>
-		<input type="text" class="title" placeholder="Product Name" maxlength="50">
-		<textarea class="description" placeholder="Description"></textarea>
+		<input type="text" placeholder="Product Name" maxlength="50">
+		
+		<textarea placeholder="Description"></textarea>
 		
 		<label for="add_to">add to</label>
 		<?= Form::select('add_to', null, array('chat' => 'Chat', 'wishlist' => 'Wish List', 'my_items' => 'My Items')) ?>
