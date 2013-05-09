b607ae286e713afa89058815f9bf743fbd81951a
ChristianBundy <me@christianbundy.com>
2013-05-08 17:18:25 -0700

    Make test font white

--- fuel/app/views/bookmark/test.php
+++ fuel/app/views/bookmark/test.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html>
-<img src="http://placehold.it/100x600/012" />
-<img src="http://placehold.it/200x500/234" />
-<img src="http://placehold.it/300x400/345" />
-<img src="http://placehold.it/400x300/456" />
-<img src="http://placehold.it/500x200/567" />
-<img src="http://placehold.it/600x100/678" />
-<img src="http://placehold.it/500x500/789" />
-<img src="http://placehold.it/400x400/890" />
-<img src="http://placehold.it/300x300/012" />
-<img src="http://placehold.it/200x200/234" />
-<img src="http://placehold.it/100x100/456" />
+
+<img src="http://placehold.it/100x600/012/fff" />
+<img src="http://placehold.it/200x500/234/fff" />
+<img src="http://placehold.it/300x400/345/fff" />
+<img src="http://placehold.it/400x300/456/fff" />
+<img src="http://placehold.it/500x200/567/fff" />
+<img src="http://placehold.it/600x100/678/fff" />
+<img src="http://placehold.it/500x500/789/fff" />
+<img src="http://placehold.it/400x400/890/fff" />
+<img src="http://placehold.it/300x300/012/fff" />
+<img src="http://placehold.it/200x200/234/fff" />
+<img src="http://placehold.it/100x100/456/fff" />
 </html>
