8d109cc98abdb788d5269da6c53e225883066e7d
Dinko <dinkoslk55@yahoo.com>
2013-05-08 23:35:46 -0500

    remove autocoplete, start styling comment box

--- fuel/app/views/chats/view.php
+++ fuel/app/views/chats/view.php
@@ -109,7 +109,7 @@
 								<div class="add-comment">
 									<?= Form::open(array('action' => "{$chat->url()}/comment")) ?>
 										<?= Html::img($user->profile_pic(24, 24)) ?>
-										<input name="comment" type="text">
+										<input name="comment" type="text" autocomplete="off">
 										<input name="chat_product_id" type="hidden" value="<?= $chat_product->id ?>">
 										<button type="submit">comment</button>
 									<?= Form::close() ?>
