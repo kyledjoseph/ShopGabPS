9e74004c64a247ddd7ad9a8d2098ff0e79b19766
Dinko <dinkoslk55@yahoo.com>
2013-05-11 10:33:28 -0500

    .

--- fuel/app/views/chats/create.php
+++ fuel/app/views/chats/create.php
@@ -1,37 +1,26 @@
 
 
-	<ul class="breadcrumb">
-					<li><?= Html::anchor('chats', 'My Chats') ?> <span class="divider">/</span></li>
-					<li class="active">Create a Chat</li>
-				</ul>
+	<section id="main-section">
 
-	<?= Form::open(array('class' => 'form-horizontal')) ?>
+		<?= Form::open(array('class' => 'form-horizontal')) ?>
 
-		<div class="control-group">
-			<label class="control-label" for="name">I'm looking for a</label>
-			<div class="controls">
-				<input type="text" name="name">
-			</div>
-		</div>
+			<ul>
+				<li>
+					<label for="name">I'm looking for a</label>
+					<input type="text" name="name">
+				</li>
+				<li>
+					<label for="description">Tell your friends a little bit about what you're looking for.</label>
+					<textarea name="description" rows="6"></textarea>
+				</li>
+				<li>
+					<label for="purchase_within">I want to purchase this within</label>
+					<?= Form::select('purchase_within', null, Model_Chat::purchase_within_fields()) ?>
+				</li>
 
-		<div class="control-group">
-			<label class="control-label" for="description">Tell your friends a little bit about what you're looking for.</label>
-			<div class="controls">
-				<textarea name="description" rows="6"></textarea>
-			</div>
-		</div>
+					<button type="submit" class="btn">Create Chat</button>
 
-		<div class="control-group">
-			<label class="control-label" for="purchase_within">I want to purchase this within</label>
-			<div class="controls">
-				<?= Form::select('purchase_within', null, Model_Chat::purchase_within_fields()) ?>
-			</div>
-		</div>
+			
+		<?= Form::close() ?>
 
-		<div class="control-group">
-			<div class="controls">
-				<button type="submit" class="btn">Create Chat</button>
-			</div>
-		</div>
-		
-	<?= Form::close() ?>
+	</section>
