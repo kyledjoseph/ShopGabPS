99690f0a69005930db34ddbf01783ba4e8243470
Dinko <dinkoslk55@yahoo.com>
2013-05-10 19:31:35 -0500

    .

--- fuel/app/views/user/register.php
+++ fuel/app/views/user/register.php
@@ -1,22 +1,22 @@
-
-		<h2>Register</h2>
+	<section id="main-section">
+		
+		<h1>Register</h1>
 
 		<?= Form::open(array('class' => 'form-horizontal')) ?>
-			<div class="control-group">
-				<label class="control-label" for="email">Email</label>
-				<div class="controls">
-					<input type="text" name="email" placeholder="Email">
-				</div>
-			</div>
-			<div class="control-group">
-				<label class="control-label" for="password">Password</label>
-				<div class="controls">
-					<input type="password" name="password" placeholder="Password">
-				</div>
-			</div>
-			<div class="control-group">
-				<div class="controls">
-					<button type="submit" class="btn">Sign in</button>
-				</div>
-			</div>
+
+		<ul>
+			<li>			
+				<label for="email">Email</label>
+				<input type="text" name="email" placeholder="Email">
+			</li>
+			<li>
+				<label for="password">Password</label>
+				<input type="password" name="password" placeholder="Password">
+			</li>
+		</ul>		
+
+		<button type="submit" class="btn">Sign in</button>
+
 		</form>
+
+	</section>
