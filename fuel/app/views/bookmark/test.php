638b189eeb72b80a97b840f35448012d2f655e79
Christian Bundy <me@christianbundy.com>
2013-05-13 18:14:41 -0700

    Remove test price, change size, and colors.

--- fuel/app/views/bookmark/test.php
+++ fuel/app/views/bookmark/test.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html>
-$5,000
-<img src="http://placehold.it/100x600/012/fff" />
-<img src="http://placehold.it/200x500/234/fff" />
-<img src="http://placehold.it/300x400/345/fff" />
-<img src="http://placehold.it/400x300/456/fff" />
-<img src="http://placehold.it/500x200/567/fff" />
-<img src="http://placehold.it/600x100/678/fff" />
-<img src="http://placehold.it/500x500/789/fff" />
-<img src="http://placehold.it/400x400/890/fff" />
-<img src="http://placehold.it/300x300/012/fff" />
-<img src="http://placehold.it/200x200/234/fff" />
+<img src="http://placehold.it/100x100/5959AB/fff" />
+<img src="http://placehold.it/100x100/234/fff" />
+<img src="http://placehold.it/100x100/345/fff" />
 <img src="http://placehold.it/100x100/456/fff" />
+<img src="http://placehold.it/100x100/567/fff" />
+<img src="http://placehold.it/100x100/678/fff" />
+<img src="http://placehold.it/100x100/70DB93/fff" />
+<img src="http://placehold.it/100x100/000080/fff" />
+<img src="http://placehold.it/100x100/EE82EE/fff" />
+<img src="http://placehold.it/100x100/FF7F50/fff" />
+<img src="http://placehold.it/100x100/2F4F4F/fff" />
 </html>
