f7ce4a93e4381c5a903bcb1ad391999e600e6086
Christian Bundy <me@christianbundy.com>
2013-05-15 13:49:57 -0700

    Change field names

--- fuel/app/views/bookmark/view.php
+++ fuel/app/views/bookmark/view.php
@@ -18,27 +18,27 @@
 			</li>
 
 			<li id="add_to">
-				<label for="add_to">add_to</label>
+				<label for="add_to">Add to:</label>
 				<?= Form::select('add_to', 'my', array('my' => 'My Quest', 'friend' => 'Friend\'s Quest', 'new' => 'New Quest')) ?>
 			</li>
 
 			<li id="friends" style="display:none">
-				<label for="add_to">friends</label>
+				<label for="add_to">Friends' Quests:</label>
 				<?= Form::select('friend', null, $user->select_friends()) ?>
 			</li>
 
 			<li id="friend_quests" style="display:none">
-				<label for="friend_quests">friend_quests</label>
+				<label for="friend_quests">Friends' Quests:</label>
 				<?= Form::select('friend_quest', null, array()) ?>
 			</li>
 
 			<li id="new_quest" style="display:none">
-				<label for="new_quest">new_quest</label>
+				<label for="new_quest">New Quest:</label>
 				<?= Form::input('new_quest') ?>
 			</li>
 
 			<li id="my_quests">
-				<label for="my_quests">my_quests</label>
+				<label for="my_quests">My Quests: </label>
 				<?= Form::select('my_quest', null, $user->select_chat()) ?>
 			</li>
 
