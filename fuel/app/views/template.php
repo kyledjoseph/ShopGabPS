6f63c9e0f47eb381a055033dbe3808e8e85e0ccc
Dinko <dinkoslk55@yahoo.com>
2013-05-20 04:34:40 -0500

    add new product bookmark

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -265,6 +265,11 @@ $(function () {
 		<h3>How do I add an item?</h3>
 
 		<a href="javascript:(function(){var e=document.createElement('script');e.src='http://beta.itemnation.com/assets/js/bookmark/parent.js?cacheblock=' + Math.floor(Math.random() * 999999999);document.getElementsByTagName('head')[0].appendChild(e)})();"><span>Add To ItemNation</span></a>
+
+		<div class="bookmark-video">
+			<h4>How does the bookmark work?</h4>
+			<iframe width="224" height="114" src="http://www.youtube.com/embed/x0ArM1rtwNI" frameborder="0" allowfullscreen></iframe>
+		</div>
 	</div>
 
 </div>
