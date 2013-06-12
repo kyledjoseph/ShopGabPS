00e5bed871f28d44a638f2f499aea5ab77040891
tmatthewsdev <tmatthewsdev@gmail.com>
2013-06-12 04:17:30 -0400

    signup modal fixes #129

--- fuel/app/views/user/modal/register.php
+++ fuel/app/views/user/modal/register.php
@@ -1,53 +1,55 @@
-<div id="registerModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="registerModalLabel" aria-hidden="true">
-	<div class="modal-header">
-	  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-	  <h3 id="loginModalLabel">Sign Up</h3>
-	</div>
-	<div class=>
-	  <div class="row">
-		<div class="span3 social-login">
-		  <?= Asset::img('social-login.png') ?>
+	<div id="registerModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="registerModalLabel" aria-hidden="true">
+		<div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+			<h3 id="loginModalLabel">Sign Up</h3>
 		</div>
-		<div class="span4 form-login">
-		  <form>
-			<div class="control-group">
-			  <label class="control-label" for="inputFullName">Full Name</label>
-			  <div class="controls">
-				<input type="text" id="inputFullName" class="full-width" placeholder="Full Name">
-			  </div>
-			</div>
-			<div class="control-group">
-			  <label class="control-label" for="inputEmail">Email</label>
-			  <div class="controls">
-				<input type="text" id="inputEmail" class="full-width" placeholder="Email">
-			  </div>
-			</div>
+		
+		<?= Form::open(array('action' => 'user/register', 'method' => 'post')) ?>
+			<div class="row">
+				<div class="span3 social-login">
+					<?= Html::anchor('auth/facebook', Asset::img('facebook-login.png')) ?>
+				</div>
+				<div class="span4 form-login">
+					<div>
+						<div class="control-group">
+							<label class="control-label" for="name">Full Name</label>
+							<div class="controls">
+								<input type="text" name="name" id="inputFullName" class="full-width" placeholder="Full Name">
+							</div>
+						</div>
+						<div class="control-group">
+							<label class="control-label" for="email">Email</label>
+							<div class="controls">
+								<input type="text" name="email" id="inputEmail" class="full-width" placeholder="Email">
+							</div>
+						</div>
 
-			<div class="control-group">
-			  <label class="control-label" for="inputPassword">Password</label>
-			  <div class="controls">
-				<input type="password" id="inputPassword" class="full-width" placeholder="Password">
-			  </div>
+						<div class="control-group">
+							<label class="control-label" for="password">Password</label>
+							<div class="controls">
+								<input type="password" name="password" id="inputPassword" class="full-width" placeholder="Password">
+							</div>
+						</div>
+						<div class="control-group">
+							<label class="control-label" for="confirm_password">Confirm Password</label>
+							<div class="controls">
+								<input type="password" name="confirm_password" id="inputPasswordAgain" class="full-width" placeholder="Confirm Password">
+							</div>
+						</div>
+						<div class="control-group">
+							<div class="controls">
+								<label class="checkbox">
+									<input type="checkbox"> Remember me
+								</label>
+							</div>
+						</div>
+					</div>
+				</div> 
 			</div>
-			<div class="control-group">
-			  <label class="control-label" for="inputPasswordAgain">Confirm Password</label>
-			  <div class="controls">
-				<input type="password" id="inputPasswordAgain" class="full-width" placeholder="Confirm Password">
-			  </div>
+			
+			<div class="modal-footer">
+				<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+				<button class="btn btn-primary">Sign Up</button>
 			</div>
-			<div class="control-group">
-			  <div class="controls">
-				<label class="checkbox">
-				  <input type="checkbox"> Remember me
-				</label>
-			  </div>
-			</div>
-		  </form>
-		</div> 
-	  </div>
-	  <div class="modal-footer">
-		<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-		<button class="btn btn-primary">Sign Up</button>
-	  </div>
+		</form>
 	</div>
-  </div>
