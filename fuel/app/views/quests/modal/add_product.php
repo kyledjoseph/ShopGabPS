ccce8db8cc8aa0312d6756fc05faf108723c3348
Christian Bundy <me@christianbundy.com>
2013-06-21 16:35:57 -0600

    Disable YouTube video

--- fuel/app/views/quests/modal/add_product.php
+++ fuel/app/views/quests/modal/add_product.php
@@ -8,7 +8,7 @@
 				<div class="span6 add-product-drag">
 					<h4 class="drag-to">Drag this to your bookmarks!</h4>
 					<a class="grab bookmarklet" href="javascript:(function(){var e=document.createElement('script');e.id='itemnation-script';e.src='http://itemnation.com/bookmark.js?cacheblock=' + Math.floor(Math.random() * 999999999);document.getElementsByTagName('head')[0].appendChild(e)})();"><img class="grab" src="<?= Uri::create('assets/img/bookmark.png') ?>" alt="Add to ItemNation" /></a>
-					<iframe width="435" height="275" src="http://www.youtube.com/embed/qDf4w9spx5U?showinfo=0&controls=0" frameborder="0" allowfullscreen></iframe>
+					<iframe width="435" height="275" src="http://www.youtube.com/embed/qDf4w9spx5U?showinfo=0" frameborder="0" disabled allowfullscreen></iframe>
 				</div>
 				<div class="span3 add-product-form">
 					<h4>Capture from the web!</h4>
