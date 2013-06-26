429688824bb0ff846c635e0dda32a44643f194fa
tmatthewsdev <tmatthewsdev@gmail.com>
2013-06-26 18:24:20 -0400

    forgot this

--- fuel/app/views/friends/index.php
+++ fuel/app/views/friends/index.php
@@ -21,6 +21,18 @@
 
 			<?php if ($this->user->is_authenticated_with('facebook')): ?>
 
+			<h4>Facebook Friends on ItemNation</h4>
+
+			<?php foreach ($this->user->get_registered_facebook_friends() as $friend): ?>
+			<li>
+				<?= Html::img($friend->photo_url()) ?>
+				<?= Html::anchor($friend->profile_url(), $friend->identifier) ?> - 
+				<?= $friend->display_name ?>
+			</li>
+			<?php endforeach; // is_authenticated_with('facebook') ?>
+
+
+
 			<h4>Facebook Friends</h4>
 
 			<?php foreach ($this->user->get_facebook_friends() as $friend): ?>
