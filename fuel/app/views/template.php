997dc7619a6fa8bbb02da3d90ea9c73606e6813a
Christian Bundy <me@christianbundy.com>
2013-07-31 12:36:41 -0700

    Add 'shop social' and darken dashboard box h3 span

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>ShopGab - </title>
+	<title>ShopGab - Shop social!</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
