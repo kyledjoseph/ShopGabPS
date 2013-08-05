87e8e61429760859ea20c47bad0f330102afe80c
Christian Bundy <me@christianbundy.com>
2013-08-04 20:39:50 -0700

    Add new ShopGab files

--- _bootstrap/js/script.js
+++
@@ -1,7 +0,0 @@
-$('[data-typer-targets]').typer();
-
-$('.comments').click(function(){
-	event.preventDefault();
-	var comment = $(this).parent().parent().parent().data('product');
-	$('.product-block[data-product='+comment+'] .comment').toggle(100);
-});
