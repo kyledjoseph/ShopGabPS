5be3f8ac6b937976d57f42074ca142c03a9e31b4
Christian Bundy <me@christianbundy.com>
2013-08-04 21:55:03 -0700

    Remove bootstrap folder, add modal forms to landing page

--- fuel/app/views/user/modal/login.php
+++ fuel/app/views/user/modal/login.php
@@ -1,46 +1,40 @@
-	<div id="loginModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
-		<div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-			<h3 id="loginModalLabel">Login</h3>
-		</div>
-		<div class="">
-			
-			
-
-			<div class="row">
-				<div class="span3 social-login">
-					<?= Html::anchor('auth/facebook?r=' . Uri::string(), Asset::img('facebook-login.png')) ?>
-				</div>
-				<div class="span4 form-login">
-					<?= Form::open(array('action' => 'login')) ?>
-					<div class="control-group">
-						<label class="control-label" for="email">Email</label>
-						<div class="controls">
-							<input type="text" name="email" class="full-width" placeholder="Email">
-						</div>
-					</div>
-					<div class="control-group">
-						<label class="control-label" for="password">Password</label>
-						<div class="controls">
-							<input type="password" name="password" class="full-width" placeholder="Password">
-						</div>
-						<span id="error_login" class="modal_error"></span>
-					</div>
-					<div class="control-group">
-						<div class="controls">
-							<label class="checkbox">
-								<input type="checkbox"> Remember me <a href="#" class="pull-right switch-to-signup">Sign Up</a>
-							</label>
-						</div>
-					</div>
-				</div>
+<div id="loginModal" class="modal fade">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title">Login</h4>
 			</div>
-			<div class="modal-footer">
-				<input type="hidden" name="redirect" value="<?= Uri::string() ?>" ?>
-				<!-- <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button> -->
-				<button id="user_login" type="submit" class="btn btn-primary">Login</button>
+			<div class="modal-body">
+					<div class="row">
+						<div class="col-12 col-sm-6 social-login">
+							<?= Html::anchor('auth/facebook?r=' . Uri::string(), Asset::img('facebook-login.png')) ?>
+						</div>
+						<div class="col-12 col-sm-6 form-login">
+							<?= Form::open(array('action' => 'login')) ?>
+							<div class="form-group">
+								<label for="exampleInputPassword">Email</label>
+								<input name="email" type="email" class="form-control" placeholder="you@email.com">
+							</div>
+							<div class="form-group">
+								<label for="password">Password</label>
+								<input name="password" type="password" class="form-control" placeholder="******">
+							</div>
+							<span id="error_login" class="modal_error"></span>
+							<div class="form-group">
+								<label for="rememberme">Remember me</label>
+								<input name="rememberme" type="checkbox"><a href="#" class="pull-right switch-to-signup">Sign Up</a>
+							</div>
+						</div>
+					</div>
+				<div class="modal-footer in-form">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<input type="hidden" name="redirect" value="<?= Uri::string() ?>" ?>
+					<button id="user_login" type="submit" class="btn btn-primary">Login</button>
+				</div>
+				<?= Form::close() ?>
 			</div>
-
-			<?= Form::close() ?>
 		</div>
 	</div>
+</div>
+</div>
--- fuel/app/views/user/modal/register.php
+++ fuel/app/views/user/modal/register.php
@@ -1,63 +1,60 @@
-	<div id="registerModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="registerModalLabel" aria-hidden="true">
-		<div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-			<h3 id="loginModalLabel">Sign Up</h3>
-		</div>
-		
-		
-			<div class="row">
-				<div class="span3 social-login">
-					<?= Html::anchor('auth/facebook', Asset::img('facebook-login.png')) ?>
-				</div>
-				<div class="span4 form-login">
+
+
+	<!-- well -->
+
+	<div id="registerModal" class="modal fade">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title">Sign Up</h4>
+			</div>
+			<div class="modal-body">
+					<div class="row">
+						<div class="col-12 col-sm-6 social-login">
+							<?= Html::anchor('auth/facebook?r=' . Uri::string(), Asset::img('facebook-login.png')) ?>
+						</div>
+						<div class="col-12 col-sm-6 form-login">
 					<?= Form::open(array('action' => 'user/register', 'method' => 'post')) ?>
 					<div>
-						<div class="control-group">
+						<div class="form-group">
 							<label class="control-label" for="name">Full Name</label>
-							<div class="controls">
-								<input type="text" name="name" id="inputFullName" class="full-width" placeholder="Full Name">
-							</div>
+							<input type="text" name="name" id="inputFullName" class="form-control" placeholder="Full Name">
 							<span id="error_name" class="modal_error"></span>
 						</div>
 
-						<div class="control-group">
+						<div class="form-group">
 							<label class="control-label" for="email">Email</label>
-							<div class="controls">
-								<input type="text" name="email" id="inputEmail" class="full-width" placeholder="Email">
-							</div>
+							<input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email">
 							<span id="error_email" class="modal_error"></span>
 						</div>
 
-						<div class="control-group">
+						<div class="form-group">
 							<label class="control-label" for="password">Password</label>
-							<div class="controls">
-								<input type="password" name="password" id="inputPassword" class="full-width" placeholder="Password">
-							</div>
+							<input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password">
 						</div>
 
-						<div class="control-group">
+						<div class="form-group">
 							<label class="control-label" for="confirm_password">Confirm Password</label>
-							<div class="controls">
-								<input type="password" name="confirm_password" id="inputPasswordAgain" class="full-width" placeholder="Confirm Password">
-							</div>
+							<input type="password" name="confirm_password" id="inputPasswordAgain" class="form-control" placeholder="Confirm Password">
 							<span id="error_password" class="modal_error"></span>
 						</div>
 						
-						<div class="control-group">
-							<div class="controls">
-								<label class="checkbox">
-									<input type="checkbox"> Remember me <a href="#" class="pull-right switch-to-signin">Sign In</a>
-								</label>
+						<div class="form-group">
+								<label for="rememberme">Remember me</label>
+								<input name="rememberme" type="checkbox"><a href="#" class="pull-right switch-to-signin">Log In</a>
 							</div>
 						</div>
-					</div>
 				</div> 
+					</div>
+				<div class="modal-footer in-form">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<input type="hidden" name="redirect" value="<?= Uri::string() ?>" ?>
+					<button id="user_login" type="submit" class="btn btn-primary">Login</button>
+				</div>
+				<?= Form::close() ?>
 			</div>
-			
-			<div class="modal-footer">
-				<input type="hidden" name="login_redirect" value="<?= Uri::string() ?>" ?>
-				<!-- <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button> -->
-				<button id="user_register" class="btn btn-primary">Sign Up</button>
-			</div>
-		</form>
+		</div>
 	</div>
+</div>
+</div>
