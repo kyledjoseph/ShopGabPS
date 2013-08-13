8cf282a0c5ead1a9c308366c1ac6823c8c7de69b
Christian Bundy <me@christianbundy.com>
2013-08-12 20:04:15 -0700

    Change bookmarklet codes

--- fuel/app/views/bookmark/install.php
+++ fuel/app/views/bookmark/install.php
@@ -1,3 +1,5 @@
 <a href="javascript:(function(){var e=document.createElement('script');e.id='itemnation-script';e.src='http://shopgab.com/bookmark.js?cacheblock=' + Math.floor(Math.random() * 999999999);document.getElementsByTagName('head')[0].appendChild(e)})();">Add To ShopGab</a>
 <br />
-<a href="javascript:(function(){var e=document.createElement('script');e.id='itemnation-script';e.src='http://shopgab.dev/bookmark.js?cacheblock=' + Math.floor(Math.random() * 999999999);document.getElementsByTagName('head')[0].appendChild(e)})();">Add To ShopGab.dev</a>
+<a href="javascript:(function(){var e=document.createElement('script');e.id='itemnation-script';e.src='http://shopgab.dev/bookmark.js?cacheblock=' + Math.floor(Math.random() * 999999999);document.getElementsByTagName('head')[0].appendChild(e)})();">Add To SG.dev</a>
+<br />
+<a href="javascript:(function(){var e=document.createElement('script');e.id='itemnation-script';e.src='http://shopgab.dev/bookmark.js?cacheblock=' + Math.floor(Math.random() * 999999999);document.getElementsByTagName('head')[0].appendChild(e)})();">Add To test.SG</a>
