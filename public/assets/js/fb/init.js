8aafc957bfcbac70c4cd9157ac71487d9661682f
tmatthewsdev <tmatthewsdev@gmail.com>
2013-08-16 19:23:12 -0400

    check fb login status

--- public/assets/js/fb/init.js
+++ public/assets/js/fb/init.js
@@ -13,12 +13,30 @@
 				xfbml      : true                                    // Look for social plugins on the page
 			});
 
-			FB.ui({method: 'apprequests',
-				message: 'My Great Request'
-			}, function() {});
 
-			//$('#loginbutton,#feedbutton').removeAttr('disabled');
-			//FB.getLoginStatus(updateStatusCallback);
+			FB.getLoginStatus(function(response) {
+				if (response.status === 'connected') {
+					// the user is logged in and has authenticated your
+					// app, and response.authResponse supplies
+					// the user's ID, a valid access token, a signed
+					// request, and the time the access token 
+					// and signed request each expire
+					var uid = response.authResponse.userID;
+					var accessToken = response.authResponse.accessToken;
+					console.log('user is logged in');
+				} else if (response.status === 'not_authorized') {
+					// the user is logged in to Facebook, 
+					// but has not authenticated your app
+					console.log('user is logged in, but not authenticated');
+				} else {
+					// the user isn't logged in to Facebook.
+					console.log('user is not logged in');
+				}
+			});
+
+			// FB.ui({method: 'apprequests',
+			// 	message: 'My Great Request'
+			// }, function() {});
 
 
 		});
