6b9e8673ef8fdf9f4d552e2906efe2d85c9fe5a1
tmatthewsdev <tmatthewsdev@gmail.com>
2013-08-16 19:18:52 -0400

    fb test

--- public/assets/js/fb/init.js
+++ public/assets/js/fb/init.js
@@ -1,32 +1,25 @@
 
-
-	window.fbAsyncInit = function() {
+	$(document).ready(function() {
 		
-		// init the FB JS SDK
-		FB.init({
-			appId      : '168874813262398',                        // App ID from the app dashboard
-			channelUrl : '//' + shopgab.domain + '/channel.html',  // Channel file for x-domain comms
-			status     : true,                                     // Check Facebook Login status
-			xfbml      : true                                      // Look for social plugins on the page
-		});
-
-		// Additional initialization code such as adding Event Listeners goes here
-		$(document).ready(function() {
+		$.ajaxSetup({ cache: true });
+		$.getScript('//connect.facebook.net/en_UK/all.js', function(){
+		
 			console.log('fb init');
 			
+			FB.init({
+				appId: '168874813262398',                            // App ID from the app dashboard
+				channelUrl: '//' + shopgab.domain + '/channel.html', // Channel file for x-domain comms
+				status     : true,                                   // Check Facebook Login status
+				xfbml      : true                                    // Look for social plugins on the page
+			});
+
 			FB.ui({method: 'apprequests',
 				message: 'My Great Request'
 			}, function() {});
 
+			//$('#loginbutton,#feedbutton').removeAttr('disabled');
+			//FB.getLoginStatus(updateStatusCallback);
+
+
 		});
-
-	};
-
-	// Load the SDK asynchronously
-	(function(d, s, id){
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) {return;}
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/all.js";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+	});
