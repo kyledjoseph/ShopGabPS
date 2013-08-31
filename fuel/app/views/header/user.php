78323f7ce471e40c5a8667aff5d55ca5089ed23a
tmatthewsdev <tmatthewsdev@gmail.com>
2013-08-31 07:45:58 -0400

    show user menu

--- fuel/app/views/header/user.php
+++ fuel/app/views/header/user.php
@@ -6,22 +6,20 @@
 
 	<!-- Navigation -->
 	<div class="col-8 col-sm-10">
-		<?php if (Fuel::$env !== 'production'): ?>
-			<ul class="landing-nav nav nav-pills pull-right">
-				<li><?= Html::anchor('/', 'My Quests') ?></li>
-				<li class="nav-image">
-					<?= Html::anchor('account', Html::img($user->get_avatar_uri()) . ' ' . $user->display_name()) ?>
-				</li>
-				<li class="dropdown">
-					<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-						<i class="icon-wrench faded"></i>
-					</a>
-					<ul class="dropdown-menu">
-						<li><?= Html::anchor('account', 'Account') ?></li>
-						<li><?= Html::anchor('logout', 'Logout') ?></li>
-					</ul>
-				</li>
-			</ul>
-		<?php endif; ?>
+		<ul class="landing-nav nav nav-pills pull-right">
+			<li><?= Html::anchor('/', 'My Quests') ?></li>
+			<li class="nav-image">
+				<?= Html::anchor('account', Html::img($user->get_avatar_uri()) . ' ' . $user->display_name()) ?>
+			</li>
+			<li class="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+					<i class="icon-wrench faded"></i>
+				</a>
+				<ul class="dropdown-menu">
+					<li><?= Html::anchor('account', 'Account') ?></li>
+					<li><?= Html::anchor('logout', 'Logout') ?></li>
+				</ul>
+			</li>
+		</ul>
 	</div>
 </div>
