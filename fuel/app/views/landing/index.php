8d39e62580e64c3d9925672562ec2590e4e5be6d
tmatthewsdev <tmatthewsdev@gmail.com>
2013-08-31 07:35:51 -0400

    login button take 2

--- fuel/app/views/landing/index.php
+++ fuel/app/views/landing/index.php
@@ -24,12 +24,10 @@ mixpanel.init("0c06e22671690f1006e02a4d071839e9");</script><!-- end Mixpanel -->
 
     <!-- Navigation -->
     <div class="col-8 col-sm-10">
-      <?php if (Fuel::$env !== 'production'): ?>
       <ul class="landing-nav nav nav-pills pull-right">
         <li><a href="<?= Uri::create('auth/facebook?r=') ?>">Login</a></li>
         <li class="active"><a href="#registerModal" data-toggle="modal">Sign Up</button></a></li>
       </ul>
-      <?php endif; ?>
     </div>
   </div>
 
