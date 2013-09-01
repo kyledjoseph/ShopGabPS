176b7bb44e435eefb11d2b6b8391d74bac8183f3
Christian Bundy <me@christianbundy.com>
2013-08-31 19:56:07 -0700

    Add a period... it's getting late

--- public/assets/js/site/quest/tour.js
+++ public/assets/js/site/quest/tour.js
@@ -2,7 +2,7 @@ if ($.cookie('old_user') == false) {
 	$('.quest-add-product').popover('show')
 	$('#fb_share').popover('show')
 
-	$('quest-add-product').click(function() {
+	$('.quest-add-product').click(function() {
 		$.cookie('old_user', 'true')
 	});
 }
