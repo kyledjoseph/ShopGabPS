8c057ce26d92d2e188f88d00da37001de68f7254
Christian Bundy <me@christianbundy.com>
2013-08-31 19:57:38 -0700

    Move off click event because bugs

--- public/assets/js/site/quest/tour.js
+++ public/assets/js/site/quest/tour.js
@@ -1,8 +1,6 @@
 if ($.cookie('old_user') == false) {
 	$('.quest-add-product').popover('show')
 	$('#fb_share').popover('show')
-
-	$('.quest-add-product').click(function() {
-		$.cookie('old_user', 'true')
-	});
+	
+	$.cookie('old_user', 'true')
 }
