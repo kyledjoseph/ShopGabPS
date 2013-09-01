a444c2639982f5943b58fe681b32774fc68555a5
Christian Bundy <me@christianbundy.com>
2013-08-31 19:54:54 -0700

    Move old_user cookie set to click event

--- public/assets/js/site/dashboard/tour.js
+++ public/assets/js/site/dashboard/tour.js
@@ -1,5 +1,4 @@
 if (!old_user) {
 	$('.tour-quest-btn').popover('show')
 	$('.tour-friends-tab').popover('show')
-	$.cookie('old_user', 'true')
 }
--- public/assets/js/site/quest/tour.js
+++ public/assets/js/site/quest/tour.js
@@ -1,4 +1,8 @@
 if ($.cookie('old_user') == false) {
 	$('.quest-add-product').popover('show')
 	$('#fb_share').popover('show')
+
+	$('quest-add-product').click(function() {
+		$.cookie('old_user', 'true')
+	});
 }
