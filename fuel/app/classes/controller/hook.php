bb8486563904636e62aa288ec7b7f421e84ce149
tmatthewsdev <tmatthewsdev@gmail.com>
2013-10-07 13:14:06 -0400

    hook test

---
+++ fuel/app/classes/controller/hook.php
@@ -0,0 +1,27 @@
+<?php
+
+class Controller_Hook extends Controller_App
+{
+	public function post_deploy()
+	{
+		// ip validation
+		$ip = Input::ip();
+		if (! Deployment_Payload::valid_github_ip($ip))
+		{
+			//throw new Exception("Invalid github hook ip address '{$ip}'", 1);
+		}
+
+		$payload_data = Input::post('payload');
+
+		// log the post data
+		$payload = new Deployment_Payload;
+		$payload->data = $payload_data;
+		$payload->ip   = $ip;
+		$payload->save();
+
+		// deploy to test and master
+		
+
+		return true;
+	}
+}
