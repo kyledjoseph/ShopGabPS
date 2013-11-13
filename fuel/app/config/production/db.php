2b6978e17daf265cd4d614945f3e1b5b163834f5
tmatthewsdev <tmatthewsdev@gmail.com>
2013-11-13 18:09:10 -0500

    new deploy production db table

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=shopgab',
+			'dsn'        => 'mysql:host=localhost;dbname=shopgab_production',
 			'username'   => 'root',
 			'password'   => 'RLJGwdbsEQ6pbJ6',
 		),
