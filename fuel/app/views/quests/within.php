dc67ce0513f1c831ecd8d5084567fdf240655fd1
tmatthewsdev <tmatthewsdev@gmail.com>
2013-11-13 17:47:10 -0500

    purchase within topdown permissions

--- fuel/app/views/quests/within.php
+++ fuel/app/views/quests/within.php
@@ -1,5 +1,8 @@
 				<span>Purchase within:</span>
 				<?= Form::open(array('id' => 'purchase_within_form', 'class' => 'inline-block submit-on-change', 'action' => $quest->url('within'))) ?>
+				
+				<?php if (isset($user) and $quest->belongs_to_user($user)): ?>
 				<?= Form::select('purchase_within', $quest->purchase_within_option(), Model_Quest::purchase_within_fields(), array('id' => 'purchase_within_value', 'class' => 'form-control')) ?>
+				<?php endif; ?>
 				<?= Form::close() ?>
 				<span id="purchase_within_text" class="faded"><?= $quest->purchase_within !== '0' ? $quest->purchase_within_text() : null ?></span>
