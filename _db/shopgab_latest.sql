3e4550b8ec77b80513782112c5c4a4c28d98374b
Goran Blazin <goran.blazin@gmail.com>
2014-04-18 09:55:20 +0200

    DB changed

--- _db/shopgab_latest.sql
+++ _db/shopgab_latest.sql
@@ -1,23 +1,3 @@
-/*
-SQLyog Community v11.31 (64 bit)
-MySQL - 5.6.14 : Database - shopgab
-*********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`shopgab` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `shopgab`;
-
-/*Table structure for table `admin_permissions` */
-
 DROP TABLE IF EXISTS `admin_permissions`;
 
 CREATE TABLE `admin_permissions` (
@@ -674,10 +654,3 @@ CREATE TABLE `users_providers` (
   PRIMARY KEY (`id`),
   KEY `parent_id` (`parent_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `users_providers` */
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
