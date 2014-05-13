a2999728c8a89e02c20d191b6db651ad03d9fc44
Goran Blazin <goran.blazin@gmail.com>
2014-05-14 00:32:12 +0200

    Bookmarklet login changed

--- fuel/app/views/bookmark/login.php
+++ fuel/app/views/bookmark/login.php
@@ -1,1 +1,24 @@
-<p><?= Html::anchor('bookmark/auth/facebook', Html::img('assets/img/facebook-login.png')) ?></p>
+<form id="login_form" action="/login" method="post">
+  <div class="type_wrapper">
+    <label>Professional <input type="radio" class="radio professional_radio_button" name="login_type" checked="checked" value="professional"/></label>
+    <label>Client <input type="radio" class="radio client_radio_button" name="login_type" value="client" /></label>
+  </div>
+  <div class="login_wrapper form_wrapper">
+    <div class="textlike_wrapper">
+      <label>
+        E-mail: <input type="email" name="email" required="true" />
+      </label>
+    </div>
+    <div class="textlike_wrapper">
+      <label>
+        Password: <input type="password" name="password" required="true" />
+      </label>
+    </div>
+    <div class="checkbox_wrapper">
+      <label>Remember me <input type="checkbox" value="1" name="remember_me" class="checkbox" /></label>
+    </div>
+    <div class="submit_wrapper">
+      <input type="submit" value="Login" class="sg-button-secondary">
+    </div>
+  </div>
+</form>
