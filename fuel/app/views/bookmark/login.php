02e78be964143f8a7d5d3dad804ed59635ca2913
Goran Blazin <goran.blazin@gmail.com>
2014-07-22 17:24:33 +0200

    Small bugfix in bookmark section

--- fuel/app/views/bookmark/login.php
+++ fuel/app/views/bookmark/login.php
@@ -1,8 +1,4 @@
 <form id="login_form" action="/login" method="post">
-  <div class="type_wrapper">
-    <label>Professional <input type="radio" class="radio professional_radio_button" name="login_type" checked="checked" value="professional"/></label>
-    <label>Client <input type="radio" class="radio client_radio_button" name="login_type" value="client" /></label>
-  </div>
   <div class="login_wrapper form_wrapper">
     <div class="textlike_wrapper">
       <label>
